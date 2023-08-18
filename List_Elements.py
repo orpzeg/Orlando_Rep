c0b0bd0c5f219ff26e953f88870ee2c24ff5400e
_RL_ND_ <135276789+orpzeg@users.noreply.github.com>
2023-08-18 12:14:53 -0400

    Update List_Elements.py

--- List_Elements.py
+++ List_Elements.py
@@ -1,21 +1,31 @@
 import os
 import time
+import win32api
 
 def list_directory_files(directory):
     file_list = []
     for filename in os.listdir(directory):
         filepath = os.path.join(directory, filename)
+        try:
+            author = win32api.GetFileVersionInfo(filepath, '\\StringFileInfo\\040601b0\\Author')
+            tags = win32api.GetFileVersionInfo(filepath, '\\StringFileInfo\\040601b0\\Tags')
+            title = win32api.GetFileVersionInfo(filepath, '\\StringFileInfo\\040601b0\\Title')
+        except:
+            author, tags, title = None, None, None
         properties = {
             'Name': filename,
             'Size':os.path.getsize(filepath),
             'Date modified': time.ctime(os.path.getmtime(filepath)),
             'Date created': time.ctime(os.path.getctime(filepath)),
-            'File type': 'Directory' if os.path.isdir(filepath) else os.path.splitext(filename)[1]
+            'File type': 'Directory' if os.path.isdir(filepath) else os.path.splitext(filename)[1],
+            'Author': author,
+            'Tags': tags,
+            'Title': title
         }
         file_list.append(properties)
     return file_list
 
-directory_path = 'D:\\Proyectos Python'
+directory_path = 'Input your Directory'
 files_details = list_directory_files(directory_path)
 for file_detail in files_details:
     print(file_detail)
