fc1652f44e5a1ddd853baafe7b2b2c310108e6bc
_RL_ND_ <135276789+orpzeg@users.noreply.github.com>
2023-08-21 15:08:17 -0400

    Update List_Elements.py

--- List_Elements.py
+++ List_Elements.py
@@ -2,30 +2,46 @@ import os
 import time
 import win32api
 
-def list_directory_files(directory):
-    file_list = []
-    for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
+class DirectoryScanner:
+    def __init__(self, directory):
+        self.directory = directory
+
+    def getFiles(self):
+        file_list = []
+        for filename in os.listdir(self.directory):
+            filepath = os.path.join(self.directory, filename)
+            file_info = FileInformation(filepath)
+            file_list.append(file_info.getData())
+        return file_list
+
+class FileInformation:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.filename = os.path.basename(filepath)
+
+    def getData(self):
         try:
-            author = win32api.GetFileVersionInfo(filepath, '\\StringFileInfo\\040601b0\\Author')
-            tags = win32api.GetFileVersionInfo(filepath, '\\StringFileInfo\\040601b0\\Tags')
-            title = win32api.GetFileVersionInfo(filepath, '\\StringFileInfo\\040601b0\\Title')
+            author = win32api.GetFileVersionInfo(self.filepath, '\\StringFileInfo\\040601b0\\Author')
+            tags = win32api.GetFileVersionInfo(self.filepath, '\\StringFileInfo\\040601b0\\Tags')
+            title = win32api.GetFileVersionInfo(self.filepath, '\\StringFileInfo\\040601b0\\Title')
         except:
             author, tags, title = None, None, None
         properties = {
-            'Name': filename,
-            'Size':os.path.getsize(filepath),
-            'Date modified': time.ctime(os.path.getmtime(filepath)),
-            'Date created': time.ctime(os.path.getctime(filepath)),
-            'File type': 'Directory' if os.path.isdir(filepath) else os.path.splitext(filename)[1],
+            'Name': self.filename,
+            'Size': os.path.getsize(self.filepath),
+            'Date modified': time.ctime(os.path.getmtime(self.filepath)),
+            'Date created': time.ctime(os.path.getctime(self.filepath)),
+            'File type': 'Directory' if os.path.isdir(self.filepath) else os.path.splitext(self.filename)[1],
             'Author': author,
             'Tags': tags,
             'Title': title
         }
-        file_list.append(properties)
-    return file_list
+        return properties
 
-directory_path = 'Input your Directory'
-files_details = list_directory_files(directory_path)
-for file_detail in files_details:
-    print(file_detail)
+directoryScanner = DirectoryScanner('D:\\Proyectos Python')
+for file in directoryScanner.getFiles():
+    print(file)
+
+for fileInfo in directoryScanner.getFiles():
+    print(fileInfo['Size'])
+
