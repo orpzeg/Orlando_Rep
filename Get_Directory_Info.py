048e6e9a5a06aa3c42b477335ea79bf7ea5be77f
_RL_ND_ <135276789+orpzeg@users.noreply.github.com>
2023-08-22 12:33:12 -0400

    Update Get_Directory_Info.py

--- Get_Directory_Info.py
+++ Get_Directory_Info.py
@@ -3,6 +3,8 @@ import time
 import win32api
 import win32com
 import win32com.client
+import argparse
+import json
 
 class DirectoryScanner:
     def __init__(self, directory):
@@ -16,13 +18,14 @@ class DirectoryScanner:
             file_list.append(file_info.getData())
         return file_list
 
+
 class FileInformation:
     def __init__(self, filepath):
         self.filepath = filepath
         self.filename = os.path.basename(filepath)
 
     def getData(self):
-        shell = win32com.client.Dispatch("Shell.Application")
+        shell = win32com.client.Dispatch('Shell.Application')
         namespace = shell.Namespace(os.path.dirname(self.filepath))
         item = namespace.ParseName(os.path.basename(self.filepath))
         try:
@@ -47,9 +50,18 @@ class FileInformation:
         }
         return properties
 
-directoryScanner = DirectoryScanner('C:\\Program Files\\JetBrains\\PyCharm Community Edition 2023.1.4\\bin')
-for file in directoryScanner.getFiles():
-    print(file)
+def main():
+    parser = argparse.ArgumentParser(description='Scan a directory and retrieve the file information.')
+    parser.add_argument('--directory', type=str, help='Path to the directory to be scanned.')
+    parser.add_argument('--output', type=str, help='Path to the output JSON file')
 
-for fileInfo in directoryScanner.getFiles():
-    print(fileInfo['Name'])
+    args = parser.parse_args()
+
+    directoryScanner = DirectoryScanner(args.directory)
+    files_info = directoryScanner.getFiles()
+
+    with open(args.output, 'w', encoding='utf-8') as f:
+        json.dump(files_info, f, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    main()
