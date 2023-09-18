951bb7980fa58060aba8bff860d1548d1147acd6
_RL_ND_ <135276789+orpzeg@users.noreply.github.com>
2023-09-17 22:23:16 -0400

    Update Test_Dir.py

--- Test_Dir.py
+++ Test_Dir.py
@@ -24,7 +24,7 @@ def demo_file_generator(directory, file_name):
         size *= 2
 
 def cmddir_file_generator(directory, command, target_directory):
-    filename = f'{command}.txt'
+    filename = f'{command.replace("/", "")}.txt'
     output_path = os.path.join(directory, filename)
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(run_commmand(f'{command} {target_directory}', 'Test'))
@@ -49,3 +49,63 @@ class TestDir(unittest.TestCase):
         command_runner = run_commmand(f'dir {self.demo_dir}', 'Test')
         self.assertEqual(file_content, command_runner)
 
+    def test_dirB(self):
+        cmddir_file_generator(self.cmddir, 'dir /B', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir B')
+        command_runner = run_commmand(f'dir /B {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirD(self):
+        cmddir_file_generator(self.cmddir, 'dir /D', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir D')
+        command_runner = run_commmand(f'dir /D {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirON(self):
+        cmddir_file_generator(self.cmddir, 'dir /ON', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir ON')
+        command_runner = run_commmand(f'dir /ON {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirOS(self):
+        cmddir_file_generator(self.cmddir, 'dir /OS', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir OS')
+        command_runner = run_commmand(f'dir /OS {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirTC(self):
+        cmddir_file_generator(self.cmddir, 'dir /TC', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir TC')
+        command_runner = run_commmand(f'dir /TC {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirTA(self):
+        cmddir_file_generator(self.cmddir, 'dir /TA', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir TA')
+        command_runner = run_commmand(f'dir /TA {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirW(self):
+        cmddir_file_generator(self.cmddir, 'dir /W', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir W')
+        command_runner = run_commmand(f'dir /W {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dir4(self):
+        cmddir_file_generator(self.cmddir, 'dir /4', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir 4')
+        command_runner = run_commmand(f'dir /4 {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def test_dirA_NOT_A(self):
+        cmddir_file_generator(self.cmddir, 'dir /A-A', self.demo_dir)  # Create the command files
+        file_content = open_files(self.cmddir, 'dir A-A')
+        command_runner = run_commmand(f'dir /A-A {self.demo_dir}', 'Test')
+        self.assertEqual(file_content, command_runner)
+
+    def tearDown(self) -> None:
+        run_commmand(f'rmdir /S /Q {self.demo_dir}')
+        run_commmand(f'rmdir /S /Q {self.cmddir}')
+
+if __name__ == '__main__':
+    unittest.main()
